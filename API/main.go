9c103c105012e6131eee92ea85666beb3532a5a2
Pauli <pau2494@gmail.com>
2023-05-22 17:20:51 -0300

    Practica CORS

--- API/main.go
+++ API/main.go
@@ -8,10 +8,16 @@ import (
 	"labora-api/API/controller"
 
 	"github.com/gorilla/mux"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 	router := mux.NewRouter()
+
+	corsOptions := handlers.AllowedMethods([]string{"GET", "POST"})
+
+	handler := handlers.CORS(corsOptions)(router)
 
 	router.HandleFunc("/items", controller.GetItems).Methods("GET")
 	router.HandleFunc("/item", controller.GetItem).Methods("GET")
@@ -19,5 +25,5 @@ func main() {
 	router.HandleFunc("/items", controller.UpdateItem).Methods("PUT")
 	router.HandleFunc("/items/", controller.DeleteItem).Methods("DELETE")
 
-	http.ListenAndServe(":8080", router)
+	http.ListenAndServe(":8080", handler)
 }
